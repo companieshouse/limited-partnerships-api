e2d723956bc9ee000718b3a92ba1f03bd31cd97b
mattch1 <mculpin@companieshouse.gov.uk>
2024-10-24 15:13:43 +0100

    LP-119 Add initial api spec

--- src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/LimitedPartnershipsApiApplication.java
+++ src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/LimitedPartnershipsApiApplication.java
@@ -1,11 +1,21 @@
 package uk.gov.companieshouse.limitedpartnershipsapi;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class LimitedPartnershipsApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(LimitedPartnershipsApiApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        // This is to prevent times being out of time by an hour during British Summer Time in MongoDB
+        // MongoDB stores UTC datetime, and LocalDate doesn't contain timezone
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 }
