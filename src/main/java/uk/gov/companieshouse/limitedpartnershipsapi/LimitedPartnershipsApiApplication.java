a83ae6637467ae18aff412bcc9945009c754c45d
mattch1 <mculpin@companieshouse.gov.uk>
2024-10-25 14:32:03 +0100

    LP-116 Add structured logging

--- src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/LimitedPartnershipsApiApplication.java
+++ src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/LimitedPartnershipsApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LimitedPartnershipsApiApplication {
+    public static final String APP_NAMESPACE = "overseas-entities-api";
     public static void main(String[] args) {
         SpringApplication.run(LimitedPartnershipsApiApplication.class, args);
     }
