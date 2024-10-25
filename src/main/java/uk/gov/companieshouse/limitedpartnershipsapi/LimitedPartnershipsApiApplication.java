3f58864737899a35c89af8630e48eecf9906978d
mattch1 <mculpin@companieshouse.gov.uk>
2024-10-25 14:59:33 +0100

    Correct namespace added debug logout

--- src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/LimitedPartnershipsApiApplication.java
+++ src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/LimitedPartnershipsApiApplication.java
@@ -2,11 +2,13 @@ package uk.gov.companieshouse.limitedpartnershipsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import uk.gov.companieshouse.limitedpartnershipsapi.utils.ApiLogger;
 
 @SpringBootApplication
 public class LimitedPartnershipsApiApplication {
-    public static final String APP_NAMESPACE = "overseas-entities-api";
+    public static final String APP_NAMESPACE = "limited-partnerships-api";
     public static void main(String[] args) {
+        ApiLogger.debug("Starting Limited Partnerships Api");
         SpringApplication.run(LimitedPartnershipsApiApplication.class, args);
     }
 }
