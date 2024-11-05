fcba01d160d6faaccc658354df5e870e6adb46c6
lduranteau <lduranteau@companieshouse.gov.uk>
2024-11-05 13:20:43 +0000

    lp-172 : add google-java-format-dependency

--- src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/LimitedPartnershipsApiApplication.java
+++ src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/LimitedPartnershipsApiApplication.java
@@ -1,24 +1,24 @@
 package uk.gov.companieshouse.limitedpartnershipsapi;
 
 import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import uk.gov.companieshouse.limitedpartnershipsapi.utils.ApiLogger;
 
-import java.util.TimeZone;
-
 @SpringBootApplication
 public class LimitedPartnershipsApiApplication {
-    public static final String APP_NAMESPACE = "limited-partnerships-api";
-    public static void main(String[] args) {
-        ApiLogger.debug("Starting Limited Partnerships API");
-        SpringApplication.run(LimitedPartnershipsApiApplication.class, args);
-    }
+  public static final String APP_NAMESPACE = "limited-partnerships-api";
 
-    @PostConstruct
-    public void init() {
-        // This is to prevent times being out of time by an hour during British Summer Time in MongoDB
-        // MongoDB stores UTC datetime, and LocalDate doesn't contain timezone
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-    }
+  public static void main(String[] args) {
+    ApiLogger.debug("Starting Limited Partnerships API");
+    SpringApplication.run(LimitedPartnershipsApiApplication.class, args);
+  }
+
+  @PostConstruct
+  public void init() {
+    // This is to prevent times being out of time by an hour during British Summer Time in MongoDB
+    // MongoDB stores UTC datetime, and LocalDate doesn't contain timezone
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+  }
 }
--- src/test/java/uk/gov/companieshouse/limitedpartnershipsapi/config/InterceptorConfigTest.java
+++ src/test/java/uk/gov/companieshouse/limitedpartnershipsapi/config/InterceptorConfigTest.java
@@ -1,5 +1,8 @@
 package uk.gov.companieshouse.limitedpartnershipsapi.config;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InOrder;
@@ -10,38 +13,26 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import uk.gov.companieshouse.limitedpartnershipsapi.interceptor.LoggingInterceptor;
 
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 @ExtendWith(MockitoExtension.class)
 class InterceptorConfigTest {
 
-    @Mock
-    private InterceptorRegistry interceptorRegistry;
+  @Mock private InterceptorRegistry interceptorRegistry;
 
-    @Mock
-    private InterceptorRegistration interceptorRegistration;
+  @Mock private InterceptorRegistration interceptorRegistration;
 
-    @Mock
-    private LoggingInterceptor loggingInterceptor;
+  @Mock private LoggingInterceptor loggingInterceptor;
 
+  @InjectMocks private InterceptorConfig interceptorConfig;
 
-    @InjectMocks
-    private InterceptorConfig interceptorConfig;
+  @Test
+  void addInterceptorsTest() {
+    when(interceptorRegistry.addInterceptor(any())).thenReturn(interceptorRegistration);
 
-    @Test
-    void addInterceptorsTest() {
-        when(interceptorRegistry.addInterceptor(any())).thenReturn(interceptorRegistration);
+    interceptorConfig.addInterceptors(interceptorRegistry);
 
-        interceptorConfig.addInterceptors(interceptorRegistry);
+    InOrder inOrder = inOrder(interceptorRegistry, interceptorRegistration);
+    inOrder.verify(interceptorRegistry).addInterceptor(loggingInterceptor);
 
-        InOrder inOrder = inOrder(interceptorRegistry, interceptorRegistration);
-        inOrder.verify(interceptorRegistry).addInterceptor(loggingInterceptor);
-
-        verify(interceptorRegistry, times(1)).addInterceptor(any());
-    }
+    verify(interceptorRegistry, times(1)).addInterceptor(any());
+  }
 }
