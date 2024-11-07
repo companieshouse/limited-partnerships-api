23d4b9ec6956c9c610459821bf999db7bd0ad956
mattch1 <mculpin@companieshouse.gov.uk>
2024-11-07 09:11:46 +0000

    LP-204 Introduced user authentication and token permissions

--- src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/config/InterceptorConfig.java
+++ src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/config/InterceptorConfig.java
@@ -4,13 +4,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import uk.gov.companieshouse.api.interceptor.InternalUserInterceptor;
+import uk.gov.companieshouse.api.interceptor.TokenPermissionsInterceptor;
+import uk.gov.companieshouse.api.interceptor.UserAuthenticationInterceptor;
+import uk.gov.companieshouse.limitedpartnershipsapi.LimitedPartnershipsApiApplication;
 import uk.gov.companieshouse.limitedpartnershipsapi.interceptor.LoggingInterceptor;
+
+import java.util.Collections;
 
 
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
     private final LoggingInterceptor loggingInterceptor;
+
 
     public InterceptorConfig(LoggingInterceptor loggingInterceptor) {
         this.loggingInterceptor = loggingInterceptor;
@@ -24,5 +31,9 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(@NonNull InterceptorRegistry registry) {
         registry.addInterceptor(loggingInterceptor);
+        registry.addInterceptor(new TokenPermissionsInterceptor());
+        registry.addInterceptor(new UserAuthenticationInterceptor(Collections.EMPTY_LIST,
+                Collections.singletonList("oauth2"),
+                new InternalUserInterceptor(LimitedPartnershipsApiApplication.APP_NAMESPACE)));
     }
 }
--- src/test/java/uk/gov/companieshouse/limitedpartnershipsapi/config/InterceptorConfigTest.java
+++ src/test/java/uk/gov/companieshouse/limitedpartnershipsapi/config/InterceptorConfigTest.java
@@ -8,6 +8,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import uk.gov.companieshouse.api.interceptor.TokenPermissionsInterceptor;
+import uk.gov.companieshouse.api.interceptor.UserAuthenticationInterceptor;
 import uk.gov.companieshouse.limitedpartnershipsapi.interceptor.LoggingInterceptor;
 
 
@@ -29,7 +31,6 @@ class InterceptorConfigTest {
     @Mock
     private LoggingInterceptor loggingInterceptor;
 
-
     @InjectMocks
     private InterceptorConfig interceptorConfig;
 
@@ -42,6 +43,9 @@ class InterceptorConfigTest {
         InOrder inOrder = inOrder(interceptorRegistry, interceptorRegistration);
         inOrder.verify(interceptorRegistry).addInterceptor(loggingInterceptor);
 
-        verify(interceptorRegistry, times(1)).addInterceptor(any());
+        inOrder.verify(interceptorRegistry).addInterceptor(any(TokenPermissionsInterceptor.class));
+        inOrder.verify(interceptorRegistry).addInterceptor(any(UserAuthenticationInterceptor.class));
+
+        verify(interceptorRegistry, times(3)).addInterceptor(any());
     }
 }
