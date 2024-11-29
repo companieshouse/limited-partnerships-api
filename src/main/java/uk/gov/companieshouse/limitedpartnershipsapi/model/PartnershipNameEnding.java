172a1c0250f81e47e84b473428281d6a6f067d3b
lduranteau <lduranteau@companieshouse.gov.uk>
2024-11-29 07:57:22 +0000

    lp-217 : fix indentation

--- src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/model/PartnershipNameEnding.java
+++ src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/model/PartnershipNameEnding.java
@@ -3,31 +3,31 @@ package uk.gov.companieshouse.limitedpartnershipsapi.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum PartnershipNameEnding {
-  LIMITED_PARTNERSHIP("Limited Partnership"),
-  LP("LP"),
-  L_DOT_P_DOT("L.P."),
-  PARTNERIAETH_CYFYNGEDIG("Partneriaeth Cyfyngedig"),
-  PC("PC"),
-  P_DOT_C_DOT("P.C.");
+    LIMITED_PARTNERSHIP("Limited Partnership"),
+    LP("LP"),
+    L_DOT_P_DOT("L.P."),
+    PARTNERIAETH_CYFYNGEDIG("Partneriaeth Cyfyngedig"),
+    PC("PC"),
+    P_DOT_C_DOT("P.C.");
 
-  private final String description;
+    private final String description;
 
-  PartnershipNameEnding(String description) {
-    this.description = description;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  @JsonCreator
-  public static PartnershipNameEnding fromDescription(String description) {
-    for (PartnershipNameEnding nameEnding : PartnershipNameEnding.values()) {
-      if (nameEnding.getDescription().equalsIgnoreCase(description)) {
-        return nameEnding;
-      }
+    PartnershipNameEnding(String description) {
+        this.description = description;
     }
-    throw new IllegalArgumentException(
-        "No PartnershipNameEnding constant with description " + description);
-  }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @JsonCreator
+    public static PartnershipNameEnding fromDescription(String description) {
+        for (PartnershipNameEnding nameEnding : PartnershipNameEnding.values()) {
+            if (nameEnding.getDescription().equalsIgnoreCase(description)) {
+                return nameEnding;
+            }
+        }
+        throw new IllegalArgumentException(
+                "No PartnershipNameEnding constant with description " + description);
+    }
 }
