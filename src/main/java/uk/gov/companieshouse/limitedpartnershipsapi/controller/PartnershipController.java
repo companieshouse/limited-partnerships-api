1fdcf64ee2fccd90c26f9a25fc32e2d1fa37d67d
hatidev <hmutasa@companieshouse.gov.uk>
2024-12-03 11:46:44 +0000

    revert controller class

--- src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/controller/PartnershipController.java
+++ src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/controller/PartnershipController.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import uk.gov.companieshouse.api.model.transaction.Transaction;
+import uk.gov.companieshouse.limitedpartnershipsapi.exception.ServiceException;
 import uk.gov.companieshouse.limitedpartnershipsapi.model.dto.LimitedPartnershipSubmissionCreatedResponseDto;
 import uk.gov.companieshouse.limitedpartnershipsapi.model.dto.LimitedPartnershipSubmissionDto;
 import uk.gov.companieshouse.limitedpartnershipsapi.service.LimitedPartnershipService;
@@ -20,6 +21,7 @@ import java.util.HashMap;
 
 import static uk.gov.companieshouse.api.util.security.EricConstants.ERIC_IDENTITY;
 import static uk.gov.companieshouse.limitedpartnershipsapi.utils.Constants.ERIC_REQUEST_ID_KEY;
+import static uk.gov.companieshouse.limitedpartnershipsapi.utils.Constants.TRANSACTION_KEY;
 import static uk.gov.companieshouse.limitedpartnershipsapi.utils.Constants.URL_PARAM_TRANSACTION_ID;
 
 @RestController
@@ -37,26 +39,26 @@ public class PartnershipController {
 
     @PostMapping
     public ResponseEntity<Object> createPartnership(
+            @RequestAttribute(TRANSACTION_KEY) Transaction transaction,
             @RequestBody LimitedPartnershipSubmissionDto limitedPartnershipSubmissionDto,
             @RequestHeader(value = ERIC_REQUEST_ID_KEY) String requestId,
             @RequestHeader(value = ERIC_IDENTITY) String userId) {
 
-        var transactionId = 12321123;
+        var transactionId = transaction.getId();
         var logMap = new HashMap<String, Object>();
         logMap.put(URL_PARAM_TRANSACTION_ID, transactionId);
-        Transaction transaction = new Transaction();
 
         try {
-            ApiLogger.infoContext(requestId, "Calling service to create Partnership Submission", logMap);
+            ApiLogger.infoContext(requestId, "Calling service to create a Limited Partnership Submission", logMap);
 
-            var submissionId = limitedPartnershipService.createLimitedPartnership(transaction,limitedPartnershipSubmissionDto, requestId, userId);
+            var submissionId = limitedPartnershipService.createLimitedPartnership(transaction, limitedPartnershipSubmissionDto, requestId, userId);
 
             var location = URI.create(String.format(URL_GET_PARTNERSHIP, transactionId, submissionId));
             var response = new LimitedPartnershipSubmissionCreatedResponseDto(submissionId);
 
             return ResponseEntity.created(location).body(response);
-        } catch (Exception e) {
-            ApiLogger.errorContext(requestId, "Error Creating Limited Partnership Submission", e, logMap);
+        } catch (ServiceException e) {
+            ApiLogger.errorContext(requestId, "Error creating Limited Partnership submission", e, logMap);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
