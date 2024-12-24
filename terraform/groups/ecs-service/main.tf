950f8968145e631de95398b4e633a11cced0f475
makhtar-ch <makhtar@companieshouse.gov.uk>
2024-12-24 13:53:00 +0000

    tidy up tf config and update modules

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,7 +19,7 @@ terraform {
 }
 
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.294"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.296"
 
   name_prefix = "${local.service_name}-${var.environment}"
   environment = var.environment
@@ -28,7 +28,7 @@ module "secrets" {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.294"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.296"
 
   # Environmental configuration
   environment             = var.environment
@@ -82,7 +82,7 @@ module "ecs-service" {
   fargate_subnets                    = local.application_subnet_ids
 
   # Cloudwatch
-  cloudwatch_alarms_enabled = var.cloudwatch_alarms_enabled
+  cloudwatch_alarms_enabled         = var.cloudwatch_alarms_enabled
   multilb_cloudwatch_alarms_enabled = var.multilb_cloudwatch_alarms_enabled
 
   # Service environment variable and secret configs
