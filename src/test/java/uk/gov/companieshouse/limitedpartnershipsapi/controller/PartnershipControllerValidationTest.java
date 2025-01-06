68a40e7ed595b1b4cdf64453e62c7e467307517f
lduranteau <lduranteau@companieshouse.gov.uk>
2025-01-06 15:18:57 +0000

    lp-373 : controller validation test

--- src/test/java/uk/gov/companieshouse/limitedpartnershipsapi/controller/PartnershipControllerValidationTest.java
+++ src/test/java/uk/gov/companieshouse/limitedpartnershipsapi/controller/PartnershipControllerValidationTest.java
@@ -1,46 +1,83 @@
 package uk.gov.companieshouse.limitedpartnershipsapi.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import uk.gov.companieshouse.api.sdk.impl.ApiClientServiceImpl;
-import uk.gov.companieshouse.limitedpartnershipsapi.interceptor.CustomUserAuthenticationInterceptor;
+import uk.gov.companieshouse.api.interceptor.TransactionInterceptor;
+import uk.gov.companieshouse.api.model.transaction.Transaction;
 import uk.gov.companieshouse.limitedpartnershipsapi.model.PartnershipNameEnding;
 import uk.gov.companieshouse.limitedpartnershipsapi.model.PartnershipType;
 import uk.gov.companieshouse.limitedpartnershipsapi.model.dto.DataDto;
 import uk.gov.companieshouse.limitedpartnershipsapi.model.dto.LimitedPartnershipSubmissionDto;
 import uk.gov.companieshouse.limitedpartnershipsapi.service.LimitedPartnershipService;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = PartnershipController.class)
+@ContextConfiguration(classes = {PartnershipController.class})
+@WebMvcTest(controllers = {PartnershipController.class})
 public class PartnershipControllerValidationTest {
+
+    private HttpHeaders httpHeaders;
+    private Transaction transaction;
+
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @MockBean
+    private TransactionInterceptor transactionInterceptor;
+
+    @MockBean
     private LimitedPartnershipService service;
 
-    @MockBean
-    private CustomUserAuthenticationInterceptor customUserAuthenticationInterceptor;
+    @BeforeEach
+    void setUp() {
+        httpHeaders = new HttpHeaders();
+        httpHeaders.add("ERIC-Access-Token", "passthrough");
+        httpHeaders.add("X-Request-Id", "123");
+        httpHeaders.add("ERIC-Identity", "123");
 
-    @MockBean
-    private ApiClientServiceImpl apiClientServiceImpl;
+        when(transactionInterceptor.preHandle(any(), any(), any())).thenReturn(true);
 
+        transaction = new Transaction();
+    }
 
-    @Disabled("This test is disabled as work is in progress")
+    @Test
+    void testCreatePartnershipShouldReturn201() throws Exception {
+
+        LimitedPartnershipSubmissionDto limitedPartnershipSubmissionDto = new LimitedPartnershipSubmissionDto();
+        DataDto dto = new DataDto();
+
+        dto.setPartnershipName("test name");
+        dto.setNameEnding(PartnershipNameEnding.LIMITED_PARTNERSHIP);
+        dto.setPartnershipType(PartnershipType.LP);
+        limitedPartnershipSubmissionDto.setData(dto);
+
+        String body = objectMapper.writeValueAsString(limitedPartnershipSubmissionDto);
+
+        mockMvc.perform(post("/transactions/863851-951242-143528/limited-partnership/partnership")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .headers(httpHeaders)
+                        .requestAttr("transaction", transaction)
+                        .content(body))
+                .andExpect(status().isCreated());
+    }
+
     @Test
     void testCreatePartnershipShouldReturnBadRequestErrorIfPartnershipNameIsLessThan1Character() throws Exception {
 
@@ -54,11 +91,12 @@ public class PartnershipControllerValidationTest {
 
         String body = objectMapper.writeValueAsString(limitedPartnershipSubmissionDto);
 
-        mvc.perform(post("/transactions/863851-951242-143528/limited-partnership/partnership")
+        mockMvc.perform(post("/transactions/863851-951242-143528/limited-partnership/partnership")
                         .contentType(MediaType.APPLICATION_JSON)
+                        .headers(httpHeaders)
+                        .requestAttr("transaction", transaction)
                         .content(body))
                 .andExpect(status().isBadRequest());
     }
-
 
 }
