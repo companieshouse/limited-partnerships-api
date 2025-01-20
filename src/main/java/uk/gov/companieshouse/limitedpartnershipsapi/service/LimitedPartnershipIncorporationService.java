78c301b802f7a7e9debec087bd2487c6672b2b5e
dlloyd1-ch <dlloyd1@companieshouse.gov.uk>
2025-01-20 16:14:42 +0000

    Initial commit for incorporation POST endpoint

---
+++ src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/service/LimitedPartnershipIncorporationService.java
@@ -0,0 +1,49 @@
+package uk.gov.companieshouse.limitedpartnershipsapi.service;
+
+import static uk.gov.companieshouse.limitedpartnershipsapi.utils.Constants.LINK_SELF;
+import static uk.gov.companieshouse.limitedpartnershipsapi.utils.Constants.URL_GET_INCORPORATION;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.companieshouse.limitedpartnershipsapi.model.dao.LimitedPartnershipIncorporationDao;
+import uk.gov.companieshouse.limitedpartnershipsapi.repository.LimitedPartnershipIncorporationRepository;
+
+@Service
+public class LimitedPartnershipIncorporationService {
+
+    public static final String LIMITED_PARTNERSHIP_REGISTRATION_KIND = "limited-partnership-registration";
+    private final LimitedPartnershipIncorporationRepository repository;
+
+    @Autowired
+    public LimitedPartnershipIncorporationService(
+            LimitedPartnershipIncorporationRepository repository) {
+        this.repository = repository;
+    }
+
+    public String createIncorporationType(String userId, String transaction) {
+        LimitedPartnershipIncorporationDao dao = new LimitedPartnershipIncorporationDao();
+        dao.getData().setKind(LIMITED_PARTNERSHIP_REGISTRATION_KIND);
+        // TODO set etag
+        dao.setCreatedAt(LocalDateTime.now());
+        dao.setCreatedBy(userId);
+        LimitedPartnershipIncorporationDao insertedIncorporation = repository.insert(dao);
+
+        String incorporationUri = getSubmissionUri(transaction, insertedIncorporation.getId());
+        updateIncorporationTypeWithSelfLink(dao, incorporationUri);
+
+        // TODO Update transaction with master_resource
+        return insertedIncorporation.getId();
+    }
+
+    private void updateIncorporationTypeWithSelfLink(LimitedPartnershipIncorporationDao incorporationDao,
+            String submissionUri) {
+        incorporationDao.setLinks(Collections.singletonMap(LINK_SELF, submissionUri));
+        repository.save(incorporationDao);
+    }
+
+    private String getSubmissionUri(String transactionId, String submissionId) {
+        return String.format(URL_GET_INCORPORATION, transactionId, submissionId);
+    }
+}
