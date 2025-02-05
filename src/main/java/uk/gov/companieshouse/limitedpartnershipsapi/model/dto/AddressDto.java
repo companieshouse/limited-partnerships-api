1cc8b576b859b00992b498661e1f9b606cb72f4e
lduranteau <lduranteau@companieshouse.gov.uk>
2025-02-05 09:25:29 +0000

    lp-472 : remove address validator to use NotNull

--- src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/model/dto/AddressDto.java
+++ src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/model/dto/AddressDto.java
@@ -1,19 +1,20 @@
 package uk.gov.companieshouse.limitedpartnershipsapi.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import uk.gov.companieshouse.limitedpartnershipsapi.model.dto.validator.ValidAddress;
 
 import static uk.gov.companieshouse.limitedpartnershipsapi.utils.Constants.MAX_SIZE;
 import static uk.gov.companieshouse.limitedpartnershipsapi.utils.Constants.MAX_SIZE_MESSAGE;
 import static uk.gov.companieshouse.limitedpartnershipsapi.utils.Constants.MIN_SIZE;
 import static uk.gov.companieshouse.limitedpartnershipsapi.utils.Constants.MIN_SIZE_MESSAGE;
 
-@ValidAddress
+
 public class AddressDto {
 
     @JsonProperty("address_line_1")
+    @NotNull
     @Size(min = MIN_SIZE, message = "address_line_1 " + MIN_SIZE_MESSAGE)
     @Size(max = MAX_SIZE, message = "address_line_1 " + MAX_SIZE_MESSAGE)
     private String addressLine1;
@@ -23,21 +24,25 @@ public class AddressDto {
     private String addressLine2;
 
     @JsonProperty("country")
+    @NotNull
     @Size(min = MIN_SIZE, message = "country " + MIN_SIZE_MESSAGE)
     @Size(max = MAX_SIZE, message = "country " + MAX_SIZE_MESSAGE)
     private String country;
 
     @JsonProperty("locality")
+    @NotNull
     @Size(min = MIN_SIZE, message = "locality " + MIN_SIZE_MESSAGE)
     @Size(max = MAX_SIZE, message = "locality " + MAX_SIZE_MESSAGE)
     private String locality;
 
     @JsonProperty("postal_code")
+    @NotNull
     @Pattern(regexp = "^[A-Za-z]{1,2}\\d[A-Za-z\\d]? ?\\d[A-Za-z]{2}$")
     @Size(max = 15, message = "postal_code " + MAX_SIZE_MESSAGE)
     private String postalCode;
 
     @JsonProperty("premises")
+    @NotNull
     @Size(min = MIN_SIZE, message = "premises " + MIN_SIZE_MESSAGE)
     @Size(max = MAX_SIZE, message = "premises " + MAX_SIZE_MESSAGE)
     private String premises;
