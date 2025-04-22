de586f1f46a999436e5eef438d82efb3596379c8
lduranteau <lduranteau@companieshouse.gov.uk>
2025-04-22 14:41:09 +0100

    lp-738 : add missing id in LimitedPartnerDto

--- src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/model/limitedpartner/dto/LimitedPartnerDto.java
+++ src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/model/limitedpartner/dto/LimitedPartnerDto.java
@@ -7,10 +7,21 @@ import jakarta.validation.Valid;
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 public class LimitedPartnerDto {
+
+    private String id;
+
     @JsonInclude(NON_NULL)
     @JsonProperty("data")
     @Valid
     private LimitedPartnerDataDto data;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public LimitedPartnerDataDto getData() {
         return data;
