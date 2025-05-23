d5365359e5958da9fc0af116942cb06fa86eab96
markpit <mpitten@companieshouse.gov.uk>
2025-05-23 15:53:00 +0100

    LP-783 Rmove unused import

--- src/test/java/uk/gov/companieshouse/limitedpartnershipsapi/service/LimitedPartnershipServiceUpdateTest.java
+++ src/test/java/uk/gov/companieshouse/limitedpartnershipsapi/service/LimitedPartnershipServiceUpdateTest.java
@@ -30,7 +30,6 @@ import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
