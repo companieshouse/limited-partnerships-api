a699d6b2305c353fd908a8307f67e897bfaf91cb
markpit <mpitten@companieshouse.gov.uk>
2025-05-30 17:55:42 +0100

    Refactor not null checks in PartnerValidator

--- src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/service/PartnerValidator.java
+++ src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/service/PartnerValidator.java
@@ -31,55 +31,32 @@ public abstract class PartnerValidator {
     }
 
     protected void addError(String className, String fieldName, String defaultMessage, BindingResult bindingResult) {
-        var fieldError = new FieldError(className, fieldName, defaultMessage);
-        bindingResult.addError(fieldError);
+        bindingResult.addError(new FieldError(className, fieldName, defaultMessage));
     }
 
-    protected void checkNotNullLegalEntity(String className, PartnerDataDto partnerDataDto,
+    protected void checkNotNullLegalEntity(String className,
+                                           PartnerDataDto partnerDataDto,
                                            BindingResult bindingResult) {
-
-        if (partnerDataDto.getLegalEntityName() == null) {
-            addError(className, PartnerDataDto.LEGAL_ENTITY_NAME_FIELD, "Legal Entity Name is required", bindingResult);
-        }
-
-        if (partnerDataDto.getLegalForm() == null) {
-            addError(className, PartnerDataDto.LEGAL_FORM_FIELD, "Legal Form is required", bindingResult);
-        }
-
-        if (partnerDataDto.getGoverningLaw() == null) {
-            addError(className, PartnerDataDto.GOVERNING_LAW_FIELD, "Governing Law is required", bindingResult);
-        }
-
-        if (partnerDataDto.getLegalEntityRegisterName() == null) {
-            addError(className, PartnerDataDto.LEGAL_ENTITY_REGISTER_NAME_FIELD, "Legal Entity Register Name is required", bindingResult);
-        }
-
-        if (partnerDataDto.getLegalEntityRegistrationLocation() == null) {
-            addError(className, PartnerDataDto.LEGAL_ENTITY_REGISTRATION_LOCATION_FIELD, "Legal Entity Registration Location is required", bindingResult);
-        }
-
-        if (partnerDataDto.getRegisteredCompanyNumber() == null) {
-            addError(className, PartnerDataDto.REGISTERED_COMPANY_NUMBER_FIELD, "Registered Company Number is required", bindingResult);
-        }
+        checkField(partnerDataDto.getLegalEntityName(), className, PartnerDataDto.LEGAL_ENTITY_NAME_FIELD, "Legal Entity Name is required", bindingResult);
+        checkField(partnerDataDto.getLegalForm(), className, PartnerDataDto.LEGAL_FORM_FIELD, "Legal Form is required", bindingResult);
+        checkField(partnerDataDto.getGoverningLaw(), className, PartnerDataDto.GOVERNING_LAW_FIELD, "Governing Law is required", bindingResult);
+        checkField(partnerDataDto.getLegalEntityRegisterName(), className, PartnerDataDto.LEGAL_ENTITY_REGISTER_NAME_FIELD, "Legal Entity Register Name is required", bindingResult);
+        checkField(partnerDataDto.getLegalEntityRegistrationLocation(), className, PartnerDataDto.LEGAL_ENTITY_REGISTRATION_LOCATION_FIELD, "Legal Entity Registration Location is required", bindingResult);
+        checkField(partnerDataDto.getRegisteredCompanyNumber(), className, PartnerDataDto.REGISTERED_COMPANY_NUMBER_FIELD, "Registered Company Number is required", bindingResult);
     }
 
-    protected void checkNotNullPerson(String className, PartnerDataDto partnerDataDto,
+    protected void checkNotNullPerson(String className,
+                                      PartnerDataDto partnerDataDto,
                                       BindingResult bindingResult) {
-        if (partnerDataDto.getForename() == null) {
-            addError(className, PartnerDataDto.FORENAME_FIELD, "Forename is required", bindingResult);
-        }
-
-        if (partnerDataDto.getSurname() == null) {
-            addError(className, PartnerDataDto.SURNAME_FIELD, "Surname is required", bindingResult);
-        }
-
-        if (partnerDataDto.getDateOfBirth() == null) {
-            addError(className, PartnerDataDto.DATE_OF_BIRTH_FIELD, "Date of birth is required", bindingResult);
-        }
-
-        if (partnerDataDto.getNationality1() == null) {
-            addError(className, PartnerDataDto.NATIONALITY1_FIELD, "Nationality1 is required", bindingResult);
-        }
+        checkField(partnerDataDto.getForename(), className, PartnerDataDto.FORENAME_FIELD, "Forename is required", bindingResult);
+        checkField(partnerDataDto.getSurname(), className, PartnerDataDto.SURNAME_FIELD, "Surname is required", bindingResult);
+        checkField(partnerDataDto.getDateOfBirth(), className, PartnerDataDto.DATE_OF_BIRTH_FIELD, "Date of birth is required", bindingResult);
+        checkField(partnerDataDto.getNationality1(), className, PartnerDataDto.NATIONALITY1_FIELD, "Nationality1 is required", bindingResult);
     }
 
+    private void checkField(Object value, String className, String fieldName, String errorMessage, BindingResult bindingResult) {
+        if (value == null) {
+            addError(className, fieldName, errorMessage, bindingResult);
+        }
+    }
 }
