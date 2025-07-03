a3fa5abb60e7fd7b8aa5f9e4868c7adfbec550b2
mattch1 <mculpin@companieshouse.gov.uk>
2025-07-03 12:51:44 +0100

    LP-972 Removed unused imports

--- src/test/java/uk/gov/companieshouse/limitedpartnershipsapi/service/LimitedPartnerServiceValidateTest.java
+++ src/test/java/uk/gov/companieshouse/limitedpartnershipsapi/service/LimitedPartnerServiceValidateTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
