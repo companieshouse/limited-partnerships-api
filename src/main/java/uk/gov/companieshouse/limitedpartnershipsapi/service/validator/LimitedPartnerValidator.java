7110a02f7ca1fc79104e15677a27fc24a2914bad
mattch1 <mculpin@companieshouse.gov.uk>
2025-10-27 14:37:06 +0000

    LP-1370 Introduce remove limited partner legal entity

--- src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/service/validator/LimitedPartnerValidator.java
+++ src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/service/validator/LimitedPartnerValidator.java
@@ -6,6 +6,7 @@ import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import uk.gov.companieshouse.api.model.transaction.Transaction;
 import uk.gov.companieshouse.api.model.validationstatus.ValidationStatusError;
@@ -153,6 +154,46 @@ public class LimitedPartnerValidator extends PartnerValidator {
         if (bindingResult.hasErrors()) {
             throw new MethodArgumentNotValidException(methodParameter, bindingResult);
         }
+    }
+
+    public void validateRemove(LimitedPartnerDto limitedPartnerDto, Transaction transaction) throws ServiceException, NoSuchMethodException, MethodArgumentNotValidException {
+        BindingResult bindingResult = new BeanPropertyBindingResult(limitedPartnerDto, LimitedPartnerDataDto.class.getName());
+
+        dtoValidation(CLASS_NAME, limitedPartnerDto, bindingResult);
+        validateCeaseDate(CLASS_NAME, transaction, limitedPartnerDto, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            var methodParameter = new MethodParameter(LimitedPartnerDataDto.class.getConstructor(), -1);
+            throw new MethodArgumentNotValidException(methodParameter, bindingResult);
+        }
+    }
+
+    /**
+     * Validate removal data for a general partner and return any validation errors as
+     * a list of {@link ValidationStatusError}.
+     * <p>
+     * call by validation-status endpoint for remove general partner after closing transaction
+     *
+     * @param limitedPartnerDto the general partner DTO to validate
+     * @param transaction       the transaction context used for validation rules
+     * @return a list of {@link ValidationStatusError} representing field validation errors;
+     * an empty list if no errors are found
+     */
+    public List<ValidationStatusError> validateRemoveStatus(LimitedPartnerDto limitedPartnerDto, Transaction transaction) throws ServiceException {
+        List<ValidationStatusError> errorsList = new ArrayList<>();
+
+        BindingResult bindingResult = new BeanPropertyBindingResult(limitedPartnerDto, LimitedPartnerDataDto.class.getName());
+
+        dtoValidation(CLASS_NAME, limitedPartnerDto, bindingResult);
+        validateCeaseDate(CLASS_NAME, transaction, limitedPartnerDto, bindingResult);
+
+        List<FieldError> errors = bindingResult.getFieldErrors();
+
+        for (FieldError error : errors) {
+            errorsList.add(validationStatus.createValidationStatusError(error.getDefaultMessage(), error.getField()));
+        }
+
+        return errorsList;
     }
 
     private void checkFieldConstraints(LimitedPartnerDto limitedPartnerDto, Transaction transaction, List<ValidationStatusError> errorsList)
