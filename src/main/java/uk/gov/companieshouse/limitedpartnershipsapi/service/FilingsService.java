5098448ec42c1a47bfa024ed4ffc218b5beee08b
lduranteau <lduranteau@companieshouse.gov.uk>
2026-02-20 10:39:27 +0000

    lp-1689 : change method name

--- src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/service/FilingsService.java
+++ src/main/java/uk/gov/companieshouse/limitedpartnershipsapi/service/FilingsService.java
@@ -90,7 +90,7 @@ public class FilingsService {
         var filing = new FilingApi();
         setFilingApiData(filing, transaction);
 
-        addCostLinkInFilingIfExistsInResource(transaction, filing);
+        addCostLinkInFilingForRefundIfExistsInResource(transaction, filing);
 
         return filing;
     }
@@ -280,12 +280,12 @@ public class FilingsService {
         setPaymentData(data, transaction);
         filing.setData(data);
 
-        addCostLinkInFilingIfExistsInResource(transaction, filing);
+        addCostLinkInFilingForRefundIfExistsInResource(transaction, filing);
 
         return filing;
     }
 
-    private static void addCostLinkInFilingIfExistsInResource(Transaction transaction, FilingApi filing) {
+    private static void addCostLinkInFilingForRefundIfExistsInResource(Transaction transaction, FilingApi filing) {
         Resource resource = transaction.getResources().values().stream().toList().getFirst();
         String cost = resource.getLinks().get(LINK_COSTS);
         if (cost != null) {
